5e3fc477f911975c918be01092c29eace141de53
hikurt <32220244+hikurt@users.noreply.github.com>
2023-02-16 11:53:05 +0100

    bad aproach

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import Card from '../UI/Card';
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../store/cart-slice';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { cartActions } from '../../store/cart-slice';
+import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id,
-      price,
-      title,
-    }));
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id,
+    //     title,
+    //     price,
+    //   })
+    // );
   };
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,18 +2,22 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState:{
+  initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          id: newItem.id, 
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -26,16 +30,15 @@ const cartSlice = createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find(item => item.id === id);
+      const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
-  }
+  },
 });
 
 export const cartActions = cartSlice.actions;
